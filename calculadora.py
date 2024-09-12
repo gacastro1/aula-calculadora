c377f6d65a239ed2962d338935656f10c8d3fee2
gacastro <gcastrowork1@gmail.com>
2024-09-12 12:09:15 -0300

    first commit

---
+++ calculadora.py
@@ -0,0 +1,51 @@
+# Calculadora Simples
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Erro! Divisão por zero."
+    return x / y
+
+def main():
+    print("Selecione a operação desejada:")
+    print("1. Adição")
+    print("2. Subtração")
+    print("3. Multiplicação")
+    print("4. Divisão")
+
+    while True:
+        choice = input("Digite o número da operação (1/2/3/4): ")
+
+        if choice in ('1', '2', '3', '4'):
+            num1 = float(input("Digite o primeiro número: "))
+            num2 = float(input("Digite o segundo número: "))
+
+            if choice == '1':
+                print(f"{num1} + {num2} = {add(num1, num2)}")
+
+            elif choice == '2':
+                print(f"{num1} - {num2} = {subtract(num1, num2)}")
+
+            elif choice == '3':
+                print(f"{num1} * {num2} = {multiply(num1, num2)}")
+
+            elif choice == '4':
+                print(f"{num1} / {num2} = {divide(num1, num2)}")
+
+        else:
+            print("Entrada inválida")
+
+        next_calculation = input("Você quer fazer outra operação? (sim/não): ")
+        if next_calculation.lower() != 'sim':
+            break
+
+if __name__ == "__main__":
+    main()
